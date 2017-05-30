8a23cfa2a8c24f978a78421ee2b1f2c02a9f6735
kalekale <kalle.lehikoinen@metosin.fi>
2017-05-30 15:38:28 +0300

    Add tests for datatypes

--- test/jsonista/core_test.clj
+++ test/jsonista/core_test.clj
@@ -28,45 +28,57 @@
     (is (= "{\"mmddyyyy\":\"00-01-70\"}" (json/to-json {:mmddyyyy (Date. 0)} {:date-format "mm-dd-yy"})))))
 
 (deftest roundrobin-tests
-  (let [data {:numbers {:integer (int 1)
-                        :long (long 2)
-                        :double (double 1.2)
-                        :float (float 3.14)
-                        :big-integer (biginteger 3)
-                        :big-decimal (bigdec 4)
-                        :ratio 3/4
-                        :short (short 5)
-                        :byte (byte 6)
-                        :big-int (bigint 7)}
-              :boolean true
-              :string "string"
-              :character \c
-              :keyword :keyword
-              :set #{1 2 3}
-              :bytes (.getBytes "bytes")
-              :uuid (UUID/fromString "fbe5a1e8-6c91-42f6-8147-6cde3188fd25")
-              :symbol 'symbol
-              :date (Date. 0)
-              :timestamp (Timestamp. 0)}
-        expected {:numbers {:integer 1
-                            :long 2
-                            :double 1.2
-                            :float 3.14
-                            :big-integer 3
-                            :big-decimal 4
-                            :ratio 0.75
-                            :short 5
-                            :byte 6
-                            :big-int 7}
-                  :boolean true
-                  :string "string"
+  (let [data     {:numbers   {:integer     (int 1)
+                              :long        (long 2)
+                              :double      (double 1.2)
+                              :float       (float 3.14)
+                              :big-integer (biginteger 3)
+                              :big-decimal (bigdec 4)
+                              :ratio       3/4
+                              :short       (short 5)
+                              :byte        (byte 6)
+                              :big-int     (bigint 7)}
+                  :boolean   true
+                  :string    "string"
+                  :character \c
+                  :keyword   :keyword
+                  :q-keyword :qualified/:keyword
+                  :set       #{1 2 3}
+                  :queue     (conj (clojure.lang.PersistentQueue/EMPTY) 1 2 3)
+                  :list      (list 1 2 3)
+                  :bytes     (.getBytes "bytes")
+                  :uuid      (UUID/fromString "fbe5a1e8-6c91-42f6-8147-6cde3188fd25")
+                  :symbol    'symbol
+                  :java-set  (doto (java.util.HashSet.) (.add 1) (.add 2) (.add 3))
+                  :java-map  (doto (java.util.HashMap.) (.put :foo "bar"))
+                  :java-list (doto (java.util.ArrayList.) (.add 1) (.add 2) (.add 3))
+                  :date      (Date. 0)
+                  :timestamp (Timestamp. 0)}
+        expected {:numbers   {:integer     1
+                              :long        2
+                              :double      1.2
+                              :float       3.14
+                              :big-integer 3
+                              :big-decimal 4
+                              :ratio       0.75
+                              :short       5
+                              :byte        6
+                              :big-int     7}
+                  :boolean   true
+                  :string    "string"
                   :character "c"
-                  :keyword "keyword"
-                  :set [1 3 2]
-                  :bytes "Ynl0ZXM="
-                  :uuid "fbe5a1e8-6c91-42f6-8147-6cde3188fd25"
-                  :symbol "symbol"
-                  :date "1970-01-01T00:00:00Z"
+                  :keyword   "keyword"
+                  :q-keyword "qualified/:keyword"
+                  :set       [1 3 2]
+                  :queue     [1 2 3]
+                  :list      [1 2 3]
+                  :bytes     "Ynl0ZXM="
+                  :uuid      "fbe5a1e8-6c91-42f6-8147-6cde3188fd25"
+                  :symbol    "symbol"
+                  :java-set  [1 2 3]
+                  :java-map  {:foo "bar"}
+                  :java-list [1 2 3]
+                  :date      "1970-01-01T00:00:00Z"
                   :timestamp "1970-01-01T00:00:00Z"}]
 
     (testing "cheshire"

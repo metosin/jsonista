64d89667f8afe3e1cff97ad3ae15b11b88aec21a
Miikka Koskinen <miikka@metosin.fi>
2017-05-30 17:53:56 +0300

    Convert the docstrings to Markdown
    For nicer online API reference.

--- src/clj/jsonista/core.clj
+++ src/clj/jsonista/core.clj
@@ -3,29 +3,29 @@
 
   Encoding example:
 
-    (require '[jsonista.core :as json])
-    (json/to-json {:hello 1})
-    ;; => \"{\\\"hello\\\":1}\"
+      (require '[jsonista.core :as json])
+      (json/to-json {:hello 1})
+      ;; => \"{\\\"hello\\\":1}\"
 
   Decoding example:
 
-    (def +data+ (json/to-json {:foo \"bar\"}))
-    (json/from-json +data+)
-    ;; => {\"foo\" \"bar\"}
+      (def +data+ (json/to-json {:foo \"bar\"}))
+      (json/from-json +data+)
+      ;; => {\"foo\" \"bar\"}
 
-  CONFIGURATION
+  ## Configuration
 
   You can configure encoding and decoding by creating a custom mapper object
   with jsonista.core/make-mapper. The options are passed in as a map.
 
   For example, to convert map keys into keywords while decoding:
 
-    (json/from-json +data+ (json/make-mapper {:keywordize? true}))
-    ;; => {:foo \"bar\"}
+      (json/from-json +data+ (json/make-mapper {:keywordize? true}))
+      ;; => {:foo \"bar\"}
 
-  See the docstring of jsonista.json/make-mapper for all available options.
+  See the docstring of [[make-mapper]] for all available options.
 
-  CUSTOM ENCODERS
+  ## Custom encoders
 
   Custom encoder is a function that take a value and a JsonGenerator object as
   the parameters. The function should call JsonGenerator methods to emit the
@@ -36,12 +36,12 @@
 
   For example, to encode java.awt.Color:
 
-     (let [encoders {java.awt.Color (fn [color gen] (.writeString gen (str color)))}
-           mapper (json/make-mapper {:encoders encoders})]
-       (json/to-json (java.awt.Color. 1 2 3) mapper))
-     ;; => \"\\\"java.awt.Color[r=1,g=2,b=3]\\\"\"
+       (let [encoders {java.awt.Color (fn [color gen] (.writeString gen (str color)))}
+             mapper (json/make-mapper {:encoders encoders})]
+         (json/to-json (java.awt.Color. 1 2 3) mapper))
+       ;; => \"\\\"java.awt.Color[r=1,g=2,b=3]\\\"\"
 
-  JSONISTA VS CHESHIRE
+  ## Jsonista vs. Cheshire
 
   jsonista uses Jackson Databind while Cheshire uses Jackson Core. In our
   benchmarks, jsonista performs better than Cheshire (take look at
@@ -68,7 +68,7 @@
 (defn- make-clojure-module
   "Create a Jackson Databind module to support Clojure datastructures.
 
-  See make-mapper docstring for the documentation of the options."
+  See [[make-mapper]] docstring for the documentation of the options."
   [{:keys [keywordize? encoders date-format]}]
   (doto (SimpleModule. "Clojure")
     (.addDeserializer java.util.List (PersistentVectorDeserializer.))
@@ -92,17 +92,21 @@
 
   The optional first parameter is a map of options. The following options are
   available:
-  Encoding options: 
-  :pretty           -- set to true use Jacksons pretty-printing defaults
-  :escape-non-ascii -- set to true to escape non ascii characters
-  :date-format      -- string for custom date formatting. If not set will use default \"yyyy-MM-dd'T'HH:mm:ss'Z'\"
-  :encoders     --  a map of custom encoders where keys should be types and values
-                    should be encoder functions
+
+  | Encoding options                                                     ||
+  | ------------------- | ------------------------------------------------- |
+  | `:pretty`           | set to true use Jacksons pretty-printing defaults |
+  | `:escape-non-ascii` | set to true to escape non ascii characters        |
+  | `:date-format`      | string for custom date formatting. default: `yyyy-MM-dd'T'HH:mm:ss'Z'`  |
+  | `:encoders`         | a map of custom encoders where keys should be types and values should be encoder functions |
+
   Encoder functions take two parameters: the value to be encoded and a
   JsonGenerator object. The function should call JsonGenerator methods to emit
   the desired JSON.
-  Decoding options:
-  :keywordize?  --  set to true to convert map keys into keywords (default: false)"
+
+  | Decoding options |                                                                |
+  | ---------------- | -------------------------------------------------------------- |
+  | `:keywordize?`   | set to true to convert map keys into keywords (default: false) |"
   ([] (make-mapper {}))
   ([options]
    (doto (ObjectMapper.)
@@ -112,20 +116,24 @@
                                                   (into-array [JsonGenerator$Feature/ESCAPE_NON_ASCII]))))))
 
 (def ^ObjectMapper +default-mapper+
-  "The default ObjectMapper instance used by jsonista.core/to-json and
-  jsonista.core/from-json unless you pass in a custom one."
+  "The default ObjectMapper instance used by [[to-json]] and [[from-json]]
+  unless you pass in a custom one."
   (make-mapper {}))
+
+(defn- ^ObjectMapper to-mapper
+  [opts-or-mapper]
+  (cond
+    (map? opts-or-mapper) (make-mapper opts-or-mapper)
+    (instance? ObjectMapper opts-or-mapper) opts-or-mapper))
 
 (defn from-json
   "Decode a value from a JSON string or InputStream.
 
-  To configure, pass in an ObjectMapper created with make-mapper, or pass in a map with options.
-  See make-mapper docstring for available options"
+  To configure, pass in an ObjectMapper created with [[make-mapper]], or pass in a map with options.
+  See [[make-mapper]] docstring for the available options."
   ([data] (from-json data +default-mapper+))
   ([data opts-or-mapper]
-   (let [mapper (cond
-                  (map? opts-or-mapper)                   (make-mapper opts-or-mapper)
-                  (instance? ObjectMapper opts-or-mapper) opts-or-mapper)]
+   (let [mapper (to-mapper opts-or-mapper)]
      (cond
        (string? data)               (.readValue ^ObjectMapper mapper ^String data ^Class Object)
        (instance? InputStream data) (.readValue ^ObjectMapper mapper ^InputStream data ^Class Object)
@@ -134,17 +142,17 @@
 (defn ^String to-json
   "Encode a value as a JSON string.
 
-  To configure, pass in an ObjectMapper created with make-mapper, or pass in a map with options.
-  See make-mapper docstring for available options"
+  To configure, pass in an ObjectMapper created with [[make-mapper]], or pass in a map with options.
+  See [[make-mapper]] docstring for the available options."
   ([object] (to-json object +default-mapper+))
   ([object opts-or-mapper]
-   (let [mapper (cond
-                  (map? opts-or-mapper) (make-mapper opts-or-mapper)
-                  (instance? ObjectMapper opts-or-mapper) opts-or-mapper)]
-     (.writeValueAsString ^ObjectMapper mapper object))))
+   (.writeValueAsString (to-mapper opts-or-mapper) object)))
 
 (defn ^String write-to
-  ([object ^Writer writer]
-   (write-to object writer +default-mapper+))
-  ([object ^Writer writer ^ObjectMapper mapper]
-   (.writeValue mapper writer object)))
+  "Encode a value as JSON and write using the provided Writer.
+
+  To configure, pass in an ObjectMapper created with [[make-mapper]], or pass in a map with options.
+  See [[make-mapper]] docstring for the available options."
+  ([object ^Writer writer] (write-to object writer +default-mapper+))
+  ([object ^Writer writer ^ObjectMapper opts-or-mapper]
+   (.writeValue (to-mapper opts-or-mapper) writer object)))

6c129f7272c9847dab6710d50a11f345dc4575cd
Tommi Reiman <tommi@iki.fi>
2018-04-30 07:40:49 +0300

    Add support fo java.time, update deps

--- project.clj
+++ project.clj
@@ -13,15 +13,15 @@
   :codox {:source-uri "http://github.com/metosin/jsonista/blob/master/{filepath}#L{line}"
           :output-path "doc"
           :metadata {:doc/format :markdown}}
-  :dependencies [[com.fasterxml.jackson.core/jackson-databind "2.9.4"]]
+  :dependencies [[com.fasterxml.jackson.core/jackson-databind "2.9.5"]
+                 [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.9.5"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [com.fasterxml.jackson.datatype/jackson-datatype-joda "2.9.5"]
                                   [cheshire "5.8.0"]
-                                  [criterium "0.4.4"]]
-                   :global-vars {*warn-on-reflection* true}}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+                                  [criterium "0.4.4"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :perf {:jvm-opts ^:replace ["-server"
                                          "-Xmx4096m"
                                          "-Dclojure.compiler.direct-linking=true"]}}
-  :aliases {"all" ["with-profile" "default:dev:default:dev,1.7:default:dev,1.8"]
+  :aliases {"all" ["with-profile" "default:dev:default:dev,1.8"]
             "perf" ["with-profile" "default,dev,perf"]})

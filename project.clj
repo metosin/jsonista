0a69f67db4c5d8ae85737a6ddf6ad3d57316b5b1
Miikka Koskinen <miikka@metosin.fi>
2019-06-08 13:46:09 +0300

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/jsonista "0.2.3-SNAPSHOT"
+(defproject metosin/jsonista "0.2.3"
   :description "Clojure library for fast JSON encoding and decoding."
   :url "https://github.com/metosin/jsonista"
   :license {:name "Eclipse Public License"

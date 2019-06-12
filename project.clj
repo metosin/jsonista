ed63df3029f6adc11b9d417b5acef6928f8acf38
Miikka Koskinen <miikka@metosin.fi>
2019-06-12 16:41:50 +0300

    Speculatively fix the Travis build

--- project.clj
+++ project.clj
@@ -14,17 +14,18 @@
   :dependencies [[com.fasterxml.jackson.core/jackson-databind "2.9.9"]
                  [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.9.9"]]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}
-             :dev      {:dependencies [[com.fasterxml.jackson.datatype/jackson-datatype-joda "2.9.9"]
+             :dev      {:dependencies [[org.clojure/clojure "1.10.1"]
+                                       [com.fasterxml.jackson.datatype/jackson-datatype-joda "2.9.9"]
                                        [cheshire "5.8.1"]
                                        [com.cognitect/transit-clj "0.8.313"]
                                        [criterium "0.4.5"]]
                         :global-vars {*warn-on-reflection* true}}
              :virgil {:plugins [[lein-virgil "0.1.9"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :perf {:jvm-opts ^:replace ["-server"
                                          "-Xmx4096m"
                                          "-Dclojure.compiler.direct-linking=true"]}}
-  :aliases {"all" ["with-profile" "default:dev:default:dev,1.8:dev,1.10"]
+  :aliases {"all" ["with-profile" "default:dev:default:dev,1.8:dev,1.9"]
             "perf" ["with-profile" "default,dev,perf"]
             "repl" ["with-profile" "default,dev,virgil" "repl"]})

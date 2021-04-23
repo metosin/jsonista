ea546ed223ff35f4ea99b065551f88b774ab8b9a
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2021-04-23 15:42:32 +0300

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/jsonista "0.3.1"
+(defproject metosin/jsonista "0.3.2"
   :description "Clojure library for fast JSON encoding and decoding."
   :url "https://github.com/metosin/jsonista"
   :license {:name "Eclipse Public License"

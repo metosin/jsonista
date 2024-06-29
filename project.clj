16a826a14c6ed417e8db90ec8ebc3d1440b26131
Tommi Reiman <tommi@iki.fi>
2024-06-29 19:10:39 +0300

    lein 0.3.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/jsonista "0.3.8"
+(defproject metosin/jsonista "0.3.9"
   :description "Clojure library for fast JSON encoding and decoding."
   :url "https://github.com/metosin/jsonista"
   :license {:name "Eclipse Public License"

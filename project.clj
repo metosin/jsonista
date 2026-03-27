cc5065244fb8d5031995d7a953514b42bf792287
Joel Kaasinen <joel.kaasinen@metosin.fi>
2026-03-27 12:18:03 +0200

    release 1.0.0 🎆

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/jsonista "0.3.14"
+(defproject metosin/jsonista "1.0.0"
   :description "Clojure library for fast JSON encoding and decoding."
   :url "https://github.com/metosin/jsonista"
   :license {:name "Eclipse Public License"
